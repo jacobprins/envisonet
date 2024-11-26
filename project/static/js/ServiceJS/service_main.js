c86b89efec08fee53faa954f4c0609a19ce8b57e
Hainzie <iansmhaines@gmail.com>
2024-11-26 17:33:34 -0500

    Update service_main.js

--- project/static/js/ServiceJS/service_main.js
+++ project/static/js/ServiceJS/service_main.js
@@ -132,6 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (file) {
             // Stores the uploaded image
             uploadedImage = file;
+            audioButton.innerHTML = 'Image Uploaded';
         }
     });
 
